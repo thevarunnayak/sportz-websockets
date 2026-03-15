7541cc8097eaf345c37cb7c55fc4ab082b62f2b0
varun.nayak <iamvarunnayak@gmail.com>
2026-03-15 19:17:01 +0530

    implement web socket server

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -51,9 +51,9 @@ matchRouter.post('/', async (req, res) => {
             status: getMatchStatus(startTime, endTime),
         }).returning();
 
-        // if(res.app.locals.broadcastMatchCreated) {
-        //     res.app.locals.broadcastMatchCreated(event);
-        // }
+        if(res.app.locals.broadcastMatchCreated) {
+            res.app.locals.broadcastMatchCreated(event);
+        }
 
         res.status(201).json({ data: event });
     } catch (e) {
